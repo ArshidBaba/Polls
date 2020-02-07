2275ec29288c812e014e06476534bfc3a8a61729
Arshid <arshidbaba93@gmail.com>
2020-02-07 17:58:56 +0530

    modified authentication scheme

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import status
+from django.contrib.auth import authenticate
 
 from .models import Poll, Choice
 from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer, UserSerializer
@@ -43,3 +44,15 @@ class UserCreate(generics.CreateAPIView):
     serializer_class = UserSerializer
     authentication_classes = ()
     permission_classes = ()
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
